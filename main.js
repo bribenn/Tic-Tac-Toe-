6f580ebc9044dc7922b248b5b199e86261742a9a
Bibek Shrestha <bibekshrestha@gmail.com>
2017-01-24 11:22:59 +0545

    Remove unnecessary loop

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
   var oWinner = 'Player O is the winner!';
   var xWinner = 'Player X is the winner!';
 
+  // Function to handle click on O and X buttons at top
   $(".btn").on('click', function() {
     $(this).removeClass('not-selected');
     $(this).siblings().addClass('not-selected');
@@ -17,6 +18,7 @@ $(document).ready(function() {
     }
   });
 
+  // Function to handle clicking of each square
   $('.square').on('click', function() {
     $(this).text(vSelected);
     if (vSelected === 'o') {
@@ -25,51 +27,46 @@ $(document).ready(function() {
       $(this).addClass('hasX').removeClass('hasO');
     }
 
-
-    $('.square').each(function() {
-      if ($("#s0").hasClass('hasO') && $("#s1").hasClass('hasO') && $("#s2").hasClass('hasO')) {
-        $(".winnerO").show();
-      } else if ($("#s3").hasClass('hasO') && $("#s4").hasClass('hasO') && $("#s5").hasClass('hasO')){
-        $(".winnerO").show();
-      } else if ($("#s6").hasClass('hasO') && $("#s7").hasClass('hasO') && $("#s8").hasClass('hasO')){
-        $(".winnerO").show();
-      } else if ($("#s0").hasClass('hasO') && $("#s3").hasClass('hasO') && $("#s6").hasClass('hasO')){
-        $(".winnerO").show();
-      } else if ($("#s1").hasClass('hasO') && $("#s4").hasClass('hasO') && $("#s7").hasClass('hasO')){
-        $(".winnerO").show();
-      } else if ($("#s2").hasClass('hasO') && $("#s5").hasClass('hasO') && $("#s8").hasClass('hasO')){
-        $(".winnerO").show();
-      } else if ($("#s0").hasClass('hasO') && $("#s4").hasClass('hasO') && $("#s8").hasClass('hasO')){
-        $(".winnerO").show();
-      } else if ($("#s2").hasClass('hasO') && $("#s4").hasClass('hasO') && $("#s6").hasClass('hasO')){
-        $(".winnerO").show();
-      } else if ($("#s0").hasClass('hasX') && $("#s1").hasClass('hasX') && $("#s2").hasClass('hasX')) {
-        $(".winnerX").show();
-      } else if ($("#s3").hasClass('hasX') && $("#s4").hasClass('hasX') && $("#s5").hasClass('hasX')){
-        $(".winnerX").show();
-      } else if ($("#s6").hasClass('hasX') && $("#s7").hasClass('hasX') && $("#s8").hasClass('hasX')){
-        $(".winnerX").show();
-      } else if ($("#s0").hasClass('hasX') && $("#s3").hasClass('hasX') && $("#s6").hasClass('hasX')){
-        $(".winnerX").show();
-      } else if ($("#s1").hasClass('hasX') && $("#s4").hasClass('hasX') && $("#s7").hasClass('hasX')){
-        $(".winnerX").show();
-      } else if ($("#s2").hasClass('hasX') && $("#s5").hasClass('hasX') && $("#s8").hasClass('hasX')){
-        $(".winnerX").show();
-      } else if ($("#s0").hasClass('hasX') && $("#s4").hasClass('hasX') && $("#s8").hasClass('hasX')){
-        $(".winnerX").show();
-      } else if ($("#s2").hasClass('hasX') && $("#s4").hasClass('hasX') && $("#s6").hasClass('hasX')){
-        $(".winnerX").show();
-      } else {
-        console.log('keep playing');
-      }
-    });
+    // Check if anyone won
+    if ($("#s0").hasClass('hasO') && $("#s1").hasClass('hasO') && $("#s2").hasClass('hasO')) {
+      $(".winnerO").show();
+    } else if ($("#s3").hasClass('hasO') && $("#s4").hasClass('hasO') && $("#s5").hasClass('hasO')){
+      $(".winnerO").show();
+    } else if ($("#s6").hasClass('hasO') && $("#s7").hasClass('hasO') && $("#s8").hasClass('hasO')){
+      $(".winnerO").show();
+    } else if ($("#s0").hasClass('hasO') && $("#s3").hasClass('hasO') && $("#s6").hasClass('hasO')){
+      $(".winnerO").show();
+    } else if ($("#s1").hasClass('hasO') && $("#s4").hasClass('hasO') && $("#s7").hasClass('hasO')){
+      $(".winnerO").show();
+    } else if ($("#s2").hasClass('hasO') && $("#s5").hasClass('hasO') && $("#s8").hasClass('hasO')){
+      $(".winnerO").show();
+    } else if ($("#s0").hasClass('hasO') && $("#s4").hasClass('hasO') && $("#s8").hasClass('hasO')){
+      $(".winnerO").show();
+    } else if ($("#s2").hasClass('hasO') && $("#s4").hasClass('hasO') && $("#s6").hasClass('hasO')){
+      $(".winnerO").show();
+    } else if ($("#s0").hasClass('hasX') && $("#s1").hasClass('hasX') && $("#s2").hasClass('hasX')) {
+      $(".winnerX").show();
+    } else if ($("#s3").hasClass('hasX') && $("#s4").hasClass('hasX') && $("#s5").hasClass('hasX')){
+      $(".winnerX").show();
+    } else if ($("#s6").hasClass('hasX') && $("#s7").hasClass('hasX') && $("#s8").hasClass('hasX')){
+      $(".winnerX").show();
+    } else if ($("#s0").hasClass('hasX') && $("#s3").hasClass('hasX') && $("#s6").hasClass('hasX')){
+      $(".winnerX").show();
+    } else if ($("#s1").hasClass('hasX') && $("#s4").hasClass('hasX') && $("#s7").hasClass('hasX')){
+      $(".winnerX").show();
+    } else if ($("#s2").hasClass('hasX') && $("#s5").hasClass('hasX') && $("#s8").hasClass('hasX')){
+      $(".winnerX").show();
+    } else if ($("#s0").hasClass('hasX') && $("#s4").hasClass('hasX') && $("#s8").hasClass('hasX')){
+      $(".winnerX").show();
+    } else if ($("#s2").hasClass('hasX') && $("#s4").hasClass('hasX') && $("#s6").hasClass('hasX')){
+      $(".winnerX").show();
+    } else {
+      console.log('keep playing');
+    }
   });
+
   $('.reset-button').on('click', function() {
     location.reload();
   });
 });
-
-
-
-
 
