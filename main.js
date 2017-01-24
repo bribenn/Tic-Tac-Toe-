2159f97413348f88d8f84b93f7b36c4c49fae009
Briana Bennett <bri.bennett89@gmail.com>
2017-01-23 23:20:16 -0500

    attempt to make code DRY:fail

--- main.js
+++ main.js
@@ -12,61 +12,46 @@ $(document).ready(function() {
 		$(this).siblings().addClass('not-selected');
 		const isO = $(this).hasClass('btn-o');
 		if (isO) {
-			vSelected = 'o';	
+			vSelected = 'O';	
 		} else {
-			vSelected = 'x'
+			vSelected = 'X';
 		}
 	});
 
 	$('.square').on('click', function() {
       $(this).text(vSelected);
-      	if (vSelected === 'o') {
+      	if (vSelected === 'O') {
       		$(this).addClass('hasO').removeClass('hasX');
       	} else {
       		$(this).addClass('hasX').removeClass('hasO');
-      	}
+     	}
 				
-
-				$('.square').each(function() {
-				    if ($("#s0").hasClass('hasO') && $("#s1").hasClass('hasO') && $("#s2").hasClass('hasO')) {
-						$(".winnerO").show();
-					} else if ($("#s3").hasClass('hasO') && $("#s4").hasClass('hasO') && $("#s5").hasClass('hasO')){
-						$(".winnerO").show();
-					} else if ($("#s6").hasClass('hasO') && $("#s7").hasClass('hasO') && $("#s8").hasClass('hasO')){
-						$(".winnerO").show();
-					} else if ($("#s0").hasClass('hasO') && $("#s3").hasClass('hasO') && $("#s6").hasClass('hasO')){
-						$(".winnerO").show();
-					} else if ($("#s1").hasClass('hasO') && $("#s4").hasClass('hasO') && $("#s7").hasClass('hasO')){
-						$(".winnerO").show();
-					} else if ($("#s2").hasClass('hasO') && $("#s5").hasClass('hasO') && $("#s8").hasClass('hasO')){
-						$(".winnerO").show();
-					} else if ($("#s0").hasClass('hasO') && $("#s4").hasClass('hasO') && $("#s8").hasClass('hasO')){
-						$(".winnerO").show();
-					} else if ($("#s2").hasClass('hasO') && $("#s4").hasClass('hasO') && $("#s6").hasClass('hasO')){
-						$(".winnerO").show();
-					} else if ($("#s0").hasClass('hasX') && $("#s1").hasClass('hasX') && $("#s2").hasClass('hasX')) {
-						$(".winnerX").show();
-					} else if ($("#s3").hasClass('hasX') && $("#s4").hasClass('hasX') && $("#s5").hasClass('hasX')){
-						$(".winnerX").show();
-					} else if ($("#s6").hasClass('hasX') && $("#s7").hasClass('hasX') && $("#s8").hasClass('hasX')){
-						$(".winnerX").show();
-					} else if ($("#s0").hasClass('hasX') && $("#s3").hasClass('hasX') && $("#s6").hasClass('hasX')){
-						$(".winnerX").show();
-					} else if ($("#s1").hasClass('hasX') && $("#s4").hasClass('hasX') && $("#s7").hasClass('hasX')){
-						$(".winnerX").show();
-					} else if ($("#s2").hasClass('hasX') && $("#s5").hasClass('hasX') && $("#s8").hasClass('hasX')){
-						$(".winnerX").show();
-					} else if ($("#s0").hasClass('hasX') && $("#s4").hasClass('hasX') && $("#s8").hasClass('hasX')){
-						$(".winnerX").show();
-					} else if ($("#s2").hasClass('hasX') && $("#s4").hasClass('hasX') && $("#s6").hasClass('hasX')){
-						$(".winnerX").show();
-					} else {
+		
+				    if ($("#s0").contents() === $("#s1").contents() && $("#s1").contents() === $("#s2").contents()) {
+				    	console.log("you win");
+						$(".winner.(contents())").show();
+					   } else if ($("#s3").contents() === $("#s4").contents() === $("#s5").contents()){
+					   	$(".winner.(contents)").show();
+					   } else if ($("#s6").contents() === $("#s7").contents() === $("#s8").contents()){
+					   	$(".winner.(contents)").show();
+					   } else if ($("#s0").contents() === $("#s3").contents() === $("#s6").contents()){
+					   	$(".winner.(contents)").show();
+					   } else if ($("#s1").contents() === $("#s4").contents() === $("#s7").contents()){
+					 	$(".winner.(contents)").show();
+					   } else if ($("#s2").contents() === $("#s5").contents() === $("#s8").contents()){
+					 	$(".winner.(contents)").show();
+					   } else if ($("#s0").contents() === $("#s4").contents() === $("#s8").contents()){
+					   	$(".winner.(contents)").show();
+					   } else if ($("#s2").contents() === $("#s4").contents() === $("#s6").contents()){
+					 	$(".winner.(contents)").show();
+					   } else {
 						console.log('keep playing');
-					}
-				});
-		});
+					   }
+			
 	$('.reset-button').on('click', function() {
 		location.reload();
+	});	
+		
 	});
 });
 	
